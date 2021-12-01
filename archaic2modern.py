74f760ddac37e2af41321dda5d2a4215214a4bab
kaldan007 <kaldantenzin@gmail>
2021-12-01 10:32:56 +0530

    alternative word extracted

--- archaic2modern.py
+++ archaic2modern.py
@@ -12,10 +12,22 @@ def to_yaml(dict_):
     return yaml.safe_dump(dict_, sort_keys=False, allow_unicode=True)
 
 def clean_word(word):
+    if word == "ཞེས་པ།":
+        return word
     unwanteds = ['ཞེས་པ་', 'དང༌', 'ཞེས་པ']
     for unwanted in unwanteds:
         word = word.replace(unwanted, '')
     return word
+
+
+def parse_modern_word(word):
+    alt_words = word.split('འམ་')
+    for walker, alt_word in enumerate(alt_words):
+        if '།' not in alt_word:
+            alt_words[walker] = alt_word + '།'
+    return alt_words
+
+
 
 def parse_archaic2modern(words):
     archaic2modern = {}
@@ -23,7 +35,7 @@ def parse_archaic2modern(words):
     for word_walker, word in enumerate(words,1):
         word = clean_word(word)
         if word_walker%2==0:
-            cur_word['modern'] = word
+            cur_word['modern'] = parse_modern_word(word)
             archaic2modern[get_unique_id()] = cur_word
             cur_word = {}
         else:
